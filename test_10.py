aaeb5e8dad5a04f56e43d660958fed4f0ff62784
Keller <keller_123@hotmail.com>
2019-12-26 16:21:43 +0800

    committed by keller

--- test_10.py
+++ test_10.py
@@ -6,14 +6,15 @@ class TestFizzBuzz(unittest.TestCase):
         self.FizzBuzz = FizzBuzz
      # test case for Fizz
      # happy flow
+     # fizz case
     def test_fizz(self):
         self.assertEqual('Fizz', FizzBuzz(3).fizzbuzz())
-
+     # Buzz case
     def test_Buzz(self):
         self.assertEqual('Buzz', FizzBuzz(5).fizzbuzz())
-
+     # FizzBuzz Case
     def test_fizzbuzz(self):
-        self.assertEqual("FizzBuzz",FizzBuzz(10).fizzbuzz())
+        self.assertEqual("FizzBuzz",FizzBuzz(15).fizzbuzz())
 
 
 if __name__ == '__main__':
