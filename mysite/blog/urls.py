aac81e79c20ce370ab641f42d51551dfbfa7dc13
Keller <keller_123@hotmail.com>
2020-01-03 18:16:12 +0800

    committed by keller

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlptterns = [
-    path('',views.blog_title),
+urlpatterns = [
+    path('',views.blog_title, name='blog_title'),
 ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-app_name = 'blog'
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    #path('blog/', include('blog.urls')),
 ]
